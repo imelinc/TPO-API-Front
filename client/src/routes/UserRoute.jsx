7b55ac9339c63aadeb8eb1e2f08454851b31a8a2
imelinc <ignaciomelinc@gmail.com>
2025-10-07 22:06:40 -0300

    Cambie algo en user route

--- client/src/routes/UserRoute.jsx
+++ client/src/routes/UserRoute.jsx
@@ -4,12 +4,10 @@ import { useAuth } from "../context/AuthContext";
 export default function UserRoute({ children }) {
     const { user } = useAuth();
 
-    // Si no hay usuario, permitir acceso (para ver productos sin estar logueado)
     if (!user) {
         return children;
     }
 
-    // Si es vendedor, redirigir al dashboard
     if (user.rol === "VENDEDOR") {
         return <Navigate to="/dashboard" replace />;
     }
